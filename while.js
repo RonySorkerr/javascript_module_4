e9954df54e35d64732014a26467393474fd067fa
RonySorkerr <mdronysarkar33299@gmail.com>
2024-01-30 08:52:47 +0600

    while looping sum

--- while.js
+++ while.js
@@ -1,24 +1,35 @@
-let num = 0;
-while (num < 3){
-    console.log('looop :', num);
-    num += 1;
-}
+// let num = 0;
+// while (num < 3){
+//     console.log('looop :', num);
+//     num += 1;
+// }
 
-let name = 'kamal';
-let times = 0;
-while (times < 5){
-    console.log(name);
-    times ++;
-}
+// let name = 'kamal';
+// let times = 0;
+// while (times < 5){
+//     console.log(name);
+//     times ++;
+// }
 
-let numb = 0;
-while(numb <= 12){
-    console.log(numb);
-    numb++;
-}
+// let numb = 0;
+// while(numb <= 12){
+//     console.log(numb);
+//     numb++;
+// }
 
-let number = 50;
-while(number <= 100){
-    console.log(number);
-    number++;
+// let number = 50;
+// while(number <= 100){
+//     console.log(number);
+//     number++;
+// }
+
+// making sum of 10 numbers
+let shonkha = 1;
+let sum = 0;
+while(shonkha <= 100){
+    // console.log(shonkha);
+    sum = sum + shonkha;
+    shonkha++;
 }
+    
+console.log('the total sum is:' , sum);
