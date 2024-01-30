8f1d97095139e1a21f80848141d6ef335cf04979
RonySorkerr <mdronysarkar33299@gmail.com>
2024-01-30 08:08:44 +0600

    while loop

---
+++ while.js
@@ -0,0 +1,12 @@
+let num = 0;
+while (num < 3){
+    console.log('looop :', num);
+    num += 1;
+}
+
+let name = 'kamal';
+let times = 0;
+while (times < 5){
+    console.log(name);
+    times ++;
+}
