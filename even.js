8ba4876ecd006ef277c6723036dae1fe3d86add8
RonySorkerr <mdronysarkar33299@gmail.com>
2024-01-30 09:09:19 +0600

    even numbers

---
+++ even.js
@@ -0,0 +1,24 @@
+// for (var i = 1; i <= 10; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
+
+// let v = 1;
+// while(v <= 10){
+//     if(v % 2 ===0){
+//         console.log('you evens are :' , v);
+//     }
+//     i++;
+// }
+
+let numb = 1;
+while(numb <= 10){
+    console.log(numb);
+
+
+    if(numb % 2 === 0){
+        console.log(numb);
+    }
+    numb++;
+}
